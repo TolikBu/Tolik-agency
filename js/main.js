ba28ee1bd34962e718937c82124ee99e711efc1a
TolikBu <tolikelement@gmail.com>
2021-04-01 07:22:27 +0500

    добавил слайдер

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ var menuButton = document.querySelector(".button-hidden");
 menuButton.addEventListener("click", function () {
   document.querySelector(".navbar-menu").classList.toggle("navbar-menu--visible");
 });
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
