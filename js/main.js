0726a96765a1773e2141f219d7e665b02ad32af9
TolikBu <tolikelement@gmail.com>
2021-04-04 11:35:37 +0500

    fix  правки от куратора

--- js/main.js
+++ js/main.js
@@ -1,44 +1,89 @@
-// $(document).ready(function () {
-//   var menuButton = $(".button-hidden");
-//   menuButton.on("click", function () {
-//     $(".navbar-menu").toggleClass("navbar-menu--visible");
-//   });
-// }
+$(document).ready(function () {
+  var menuButton = document.querySelector(".navbar-menu__link-button");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".navbar-menu__hidden")
+      .classList.toggle("navbar-menu__hidden--visible");
+  });
 
-var menuButton = document.querySelector(".button-hidden");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-menu")
-    .classList.toggle("navbar-menu--visible");
-});
+  var menuButton = document.querySelector(".button-hidden");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".navbar-menu")
+      .classList.toggle("navbar-menu--visible");
+    document.querySelector("body").classList.toggle("stopscroll");
+  });
 
-const portfolioSwiper = new Swiper(".portfolio-slider", {
-  // Optional parameters
-  loop: true,
-  slidesPerView: "auto",
+  const portfolioSwiper = new Swiper(".portfolio-slider", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 30,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".portfolio-heading__button--next",
-    prevEl: ".portfolio-heading__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".portfolio-heading__button--next",
+      prevEl: ".portfolio-heading__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const reviewsSwiper = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  slidesPerView: "auto",
-  
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-heading__button--next",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+  const reviewsSwiper = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 30,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-heading__button--next",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    $(this).attr("data-href");
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Введите ваше имя",
+          minlength: "Имя должно содержать не менее 2 символов",
+        },
+        email: {
+          required: "Email очень важен",
+          email: "Email должен быть в стиле name@domain.com",
+        },
+        phone: {
+          required: "Телефон требуется для связи",
+          minlength: "Номер должен содержать минимум 11 цифр",
+        },
+      },
+    });
+  });
+
+  $("input[name=phone]").mask("+7 (000) 000 0000");
 });
---
+++ send.php
@@ -0,0 +1,60 @@
+<?php
+include ('phpmailer/password.php');
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$mail = $_POST['email'];
+$message = $_POST['message'];
+
+
+$title = "Новый запрос с Tolik-agency";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>электронная почта:</b> $email<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message";
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'anatoliybukharov15@gmail.com'; // Логин на почте
+    $mail->Password   = $password; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('anatoliybukharov15@gmail.com', 'Анатолий Бухаров'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('tolikelement@gmail.com');  
+    // $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: index.html');
